deaa073eca19dd36430cf4debd0700820b033b9a
ratspin <6210110013@psu.ac.th>
2022-08-17 14:10:14 +0700

    student dialog box

--- src/pages/student.js
+++ src/pages/student.js
@@ -1,7 +1,12 @@
-import * as React from "react"
-import Layout from "../components/layout"
+import  React, { useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { getImage, GatsbyImage } from "gatsby-plugin-image"
+import Layout from "../components/layout"
+import {SeeMoreText,IngredientsText,RecipeName,RecipeContainer,Container,RecipeListContainer} from '../styled_components/styled'
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
 
 export default function StudentPage (){
   const data = useStaticQuery(graphql`
@@ -18,35 +23,72 @@ export default function StudentPage (){
 				      }
 			      }
           }
+          register{
+            subject_id{
+              sj_id
+              sj_name
+              time 
+              day
+              section
+            }
+          }
         }
       }
     }
   `)
+
   return (
     <Layout>
-        <section className="py-5 text-center container">
-          <div className="row py-lg-5">
+    <Container>
+      <RecipeListContainer>
             {data.directus.student.map(student => (
               <Profile data={student} key={student.std_id}></Profile>
             ))}
-          </div>
-      </section>
+      </RecipeListContainer>
+    </Container>
     </Layout>
   )
 }
 
-
 const Profile = ({ data }) => {
   const img = getImage(data.std_img.imageFile)
+  const [show, setShow] = useState("");
+  const register = data.register
+
   return (
-      <div className="card" style={{width: '18rem', margin: 12}}>
-          <div className="card-body">
-              <GatsbyImage image={img}/>
-              <div className="card-body">
-                  <h5 className="card-title">{data.std_name} </h5>
-                  <h6 className="card-text">{data.std_id}</h6>
-              </div>  
-          </div>
-      </div>
+      <RecipeContainer>
+        <Dialog onClose={() => console.log("adsadad")} aria-labelledby="simple-dialog-title" open={!!show}>
+          <DialogTitle>Subject</DialogTitle>
+          <DialogContent>
+            <table>
+              <thead text-align = "center">
+                <th>Subject ID</th>
+                <th>Subject Name</th>
+                <th>Section</th>
+                <th>Day</th>
+                <th>Time</th>
+              </thead>
+              <tbody>
+                {register.map(r => (
+                  <tr >
+                    <td>{r.subject_id.sj_id}</td>
+                    <td>{r.subject_id.sj_name}</td>
+                    <td>{r.subject_id.section}</td>
+                    <td>{r.subject_id.day}</td>
+                    <td>{r.subject_id.time}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </DialogContent>
+          <DialogActions>
+            <SeeMoreText onClick={() => setShow("")}>Close</SeeMoreText>
+          </DialogActions>
+        </Dialog>
+          <GatsbyImage image={img} />
+          <RecipeName>{data.std_name}</RecipeName>
+          <RecipeName>{data.std_id}</RecipeName>
+          <IngredientsText onClick={() => setShow(!show)}>Subject</IngredientsText>  
+      </RecipeContainer>
   )
 }
