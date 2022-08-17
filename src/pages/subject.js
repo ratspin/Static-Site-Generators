b9fd7329ad588ccaa8e0f81ab6517365c592f5c4
ratspin <6210110013@psu.ac.th>
2022-08-17 09:45:36 +0700

    page student

--- src/pages/subject.js
+++ src/pages/subject.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+export const Head = () => <Seo title="Home" />
 const AboutPage = () => (
   <Layout>
     <Seo title="About Gatsby Bootsrap 5 starter" />
